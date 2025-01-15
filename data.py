6ab574a4d9b702dcf9bc5283bc2be83317708e12
Антон Лукин <shahter86@mail.ru>
2025-01-15 13:32:04 +0300

    --amend

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ BUN = {'name': 'Булка', 'price': 10.5}
 INGREDIENT = {'ingredient_type': 'Начинка', 'name': 'Гуакамоле', 'price': 20.0}
 
 BUNS_DATA = [('black bun', 100), ('white bun', 200), ('red bun', 300)]
-INGREDIENT_DATA = [
+INGREDIENTS_DATA = [
     (INGREDIENT_TYPE_SAUCE, "hot sauce", 100),
     (INGREDIENT_TYPE_SAUCE, "sour cream", 200),
     (INGREDIENT_TYPE_SAUCE, "chili sauce", 300),
